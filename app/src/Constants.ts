09beda7e3872a9aacbae578b0b905056aa2c3410
Kitaev <klim.kitaev@gmail.com>
2016-11-21 17:52:07 +0300

    constants

---
+++ app/src/Constants.ts
@@ -0,0 +1,7 @@
+export default class Constants{
+    readonly REQUEST_AIRPORTS = 'request airports';
+    readonly RECEIVE_AIRPORTS = 'receive airports';
+    readonly CHOOSE_AIRPORT = 'choose airport';
+    readonly REQUEST_TICKETS = 'request tickets';
+    readonly RECEIVE_TICKETS = 'receive tickets';
+}
